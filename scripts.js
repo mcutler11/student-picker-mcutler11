fc688e59236b1fb1f1c7a04918f683ca21762b05
Michael Cutler <michael.cutler11@yahoo.com>
2019-03-20 13:43:06 -0500

    Initialize the button code.

--- scripts.js
+++ scripts.js
@@ -2,11 +2,17 @@
  * 1. LISTEN for and HANDLE the 'submit' event.
  * 2. QUERY the DOCUMENT for the <form> fields and store those VALUES in variable(s). -- DONE
  * 3. Add fname and lname into its own object. -- DONE
- * 4. Push the new object onto an Array
+ * 4. Push the new object onto an Array -- DONE
  * 5. Randomly select a 'student Object' from the Array and console.log().
  */
 
-const students = [];
+const students = [
+  { fname: "Michael", lname: "Cutler" },
+  { fname: "Bernard", lname: "Matthews" },
+  { fname: "Amelie", lname: "Bouchard" },
+  { fname: "Edward", lname: "Russo" },
+  { fname: "Camille", lname: "Schneider" }
+];
 
 function studentCreator(fname, lname) {
 
@@ -24,4 +30,8 @@ document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
   students.push(studentCreator(fname, lname));
   console.log(students);
+});
+
+document.querySelector('button').addEventListener('click', () => {
+  console.log("Placeholder pls.");
 });
