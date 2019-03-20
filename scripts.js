597a059b41156237dccb10ef0bed46ed5ae8e65f
Michael Cutler <michael.cutler11@yahoo.com>
2019-03-20 13:20:09 -0500

    Push objects into array.

--- scripts.js
+++ scripts.js
@@ -1,23 +1,28 @@
 /**
  * 1. LISTEN for and HANDLE the 'submit' event.
  * 2. QUERY the DOCUMENT for the <form> fields and store those VALUES in variable(s). -- DONE
- * 3. Add fname and lname into its own object.
+ * 3. Add fname and lname into its own object. -- DONE
  * 4. Push the new object onto an Array
  * 5. Randomly select a 'student Object' from the Array and console.log().
  */
 
-function submitHandler(event) {
-  event.preventDefault();
+const students = [];
+
+function studentCreator() {
 
   const fname = document.querySelector('#fname').value;
   const lname = document.querySelector('#lname').value;
 
   const student = {
-    firstName: fname,
-    lastName: lname
-  };
+    fname,
+    lname
+  }
 
-  console.log(student);
+  return student;
 }
 
-document.querySelector("form").addEventListener("submit", submitHandler);
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  students.push(studentCreator());
+  console.log(students);
+});
