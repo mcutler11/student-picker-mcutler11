d09224bb91c342b00db734ed0a72f93a0dd17b65
Michael Cutler <michael.cutler11@yahoo.com>
2019-03-20 12:52:56 -0500

    Create student object.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,23 @@
 /**
  * 1. LISTEN for and HANDLE the 'submit' event.
- * 2. QUERY the DOCUMENT for the <form> fields and store those VALUES in variable(s).
+ * 2. QUERY the DOCUMENT for the <form> fields and store those VALUES in variable(s). -- DONE
  * 3. Add fname and lname into its own object.
  * 4. Push the new object onto an Array
  * 5. Randomly select a 'student Object' from the Array and console.log().
  */
 
-function SubmitHandler(event) {
+function submitHandler(event) {
   event.preventDefault();
 
   const fname = document.querySelector('#fname').value;
   const lname = document.querySelector('#lname').value;
+
+  const student = {
+    firstName: fname,
+    lastName: lname
+  };
+
+  console.log(student);
 }
 
-document.querySelector('form').addEventListener("submit", submitHandler);
+document.querySelector("form").addEventListener("submit", submitHandler);
