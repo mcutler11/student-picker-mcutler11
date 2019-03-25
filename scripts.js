c739dc4c7ac7cf897337c59cae6959627fb58030
Michael Cutler <michael.cutler11@yahoo.com>
2019-03-25 12:49:07 -0500

    Clean up your mess.

--- scripts.js
+++ scripts.js
@@ -1,11 +1,3 @@
-/**
- * 1. LISTEN for and HANDLE the 'submit' event.
- * 2. QUERY the DOCUMENT for the <form> fields and store those VALUES in variable(s). -- DONE
- * 3. Add fname and lname into its own object. -- DONE
- * 4. Push the new object onto an Array -- DONE
- * 5. Randomly select a 'student Object' from the Array and console.log().
- */
-
 const students = [
   { fname: "Michael", lname: "Cutler" },
   { fname: "Bernard", lname: "Matthews" },
@@ -24,7 +16,7 @@ function studentCreator(fname, lname) {
   return student;
 }
 
-document.querySelector("form").addEventListener("submit", (e) => {
+document.querySelector("form").addEventListener("submit", e => {
   const fname = document.querySelector('#fname').value;
   const lname = document.querySelector('#lname').value;
   e.preventDefault();
@@ -32,6 +24,6 @@ document.querySelector("form").addEventListener("submit", (e) => {
   console.log(students);
 });
 
-document.querySelector('button').addEventListener('click', () => {
-  console.log("Placeholder pls.");
+document.querySelector("button").addEventListener("click", () => {
+  console.log(students[students[]]);
 });
