730caba9e2d3a0fb9dd352aabe7a8dec24bc222b
Michael Cutler <michael.cutler11@yahoo.com>
2019-03-25 13:08:50 -0500

    Add random number functionality.

--- scripts.js
+++ scripts.js
@@ -7,23 +7,26 @@ const students = [
 ];
 
 function studentCreator(fname, lname) {
-
   const student = {
     fname,
     lname
-  }
+  };
 
   return student;
 }
 
+const getRandomIntInclusive = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
 document.querySelector("form").addEventListener("submit", e => {
-  const fname = document.querySelector('#fname').value;
-  const lname = document.querySelector('#lname').value;
   e.preventDefault();
+
+  const fname = document.querySelector("#fname").value;
+  const lname = document.querySelector("#lname").value;
+
   students.push(studentCreator(fname, lname));
-  console.log(students);
 });
 
 document.querySelector("button").addEventListener("click", () => {
-  console.log(students[students[]]);
+  console.log(getRandomIntInclusive(1, 10));
 });
