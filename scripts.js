7ccf2f6e5a6d484c30c326277b2b865a2e535d09
Michael Cutler <michael.cutler11@yahoo.com>
2019-04-24 13:46:19 -0500

    Refactor and comment.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-const btns = document.querySelectorAll("button:not(#pick-student)");
+// Grab all buttons that are not marked as #pick-student.
+const ECbtns = document.querySelectorAll("button:not(#pick-student)");
 
 const inAnimations = [
   "bounceIn",
@@ -36,14 +37,7 @@ const inAnimations = [
   "rollIn"
 ];
 
-// const students = [
-//   { fname: "Michael", lname: "Cutler" },
-//   { fname: "Bernard", lname: "Matthews" },
-//   { fname: "Amelie", lname: "Bouchard" },
-//   { fname: "Edward", lname: "Russo" },
-//   { fname: "Camille", lname: "Schneider" }
-// ];
-
+// We need a place to store new instances of students created by our Student CONSTRUCTOR FXN.
 const students = [];
 
 let randomAnimation;
@@ -51,11 +45,15 @@ let chosen;
 
 const p = document.querySelector("p");
 
+// Function Constructor using the 'new' keyword.
 function Student(fname, lname) {
   // These properties are assigned to a NEW instance of any student - when we use the 'new' keyword
   this.fname = fname;
   this.lname = lname;
   this.pts = 0;
+
+  // NEXT:  Move this METHOD (fxn. that belongs to a specific Constructor) into a PROTOTYPE.
+  // There is no need for each and every student to keep a duplicate of this method.
   this.updatePts = function(points) {
     this.pts += points;
     console.log(chosen);
@@ -86,11 +84,10 @@ document.querySelector("#pick-student").addEventListener("click", () => {
   p.textContent = `Our lucky student is: ${chosen.fname} ${chosen.lname}`;
 
   p.classList.remove("is-hidden");
-
   p.classList.add(randomAnimation);
 });
 
-btns.forEach(btn =>
+ECbtns.forEach(btn =>
   btn.addEventListener("click", event => {
     const points = Number(event.target.textContent);
     chosen.updatePts(points);
