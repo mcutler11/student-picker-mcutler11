458b34ebade5f62a448fbc40d382d0ea452942d5
Michael Cutler <michael.cutler11@yahoo.com>
2019-05-01 13:08:43 -0500

    Add basic solution to for students 1-21 with a note about app limitation.

--- scripts.js
+++ scripts.js
@@ -60,32 +60,39 @@ function Student(fname, lname) {
   };
 }
 
+function getCountStr(n) {
+  // Greater than 3 or less than 21?  Append -th
+  // For other numbers, grab the last digit of the num passed
+  // Use a switch to evaluate the last digit
+  if (n > 3 && n < 21) return `${n}th`;
+  switch (n % 10) {
+    case 1:
+      return `${n}st`;
+    case 2:
+      return `${n}nd`;
+    case 3:
+      return `${n}st`;
+    default:
+      return `${n}th`;
+  }
+}
+
 const getRandomIntInclusive = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
 
+// Add student upon submit
 document.querySelector("form").addEventListener("submit", e => {
   e.preventDefault();
 
   const fname = document.querySelector("#fname").value;
   const lname = document.querySelector("#lname").value;
 
+  // Create a NEW student using Fxn. Constructor and 'push' onto our Array.
   students.push(new Student(fname, lname));
 
-  function getCountStudents(n) {
-    if (n > 3 && n < 21) return "th";
-    switch (n % 10) {
-      case 1:
-        return "st";
-      case 2:
-        return "nd";
-      case 3:
-        return "rd";
-      default:
-        return "th";
-    }
-  }
+  p.classList.remove("is-hidden");
 
-  p.textContent = `Added the  ${students.length}${getCountStudents(
+  p.textContent = `Added the  ${getCountStr(
     students.length
   )} student: ${fname} ${fname}`;
 });
